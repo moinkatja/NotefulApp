2a2a1457c0ec4dd34f36aa650247da4c2943d659
Katja <55636811+moinkatja@users.noreply.github.com>
2020-07-29 21:56:05 +0200

    Add files via upload

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-=======
-import React, {Component} from 'react';
-import {Route, Link} from 'react-router-dom';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
->>>>>>> 0330a561bc6e4c5c0ea5f03b15f36d2a131eb3e7
 import NoteListNav from '../NoteListNav/NoteListNav';
 import NotePageNav from '../NotePageNav/NotePageNav';
 import NoteListMain from '../NoteListMain/NoteListMain';
@@ -16,10 +10,6 @@ import AddNote from "../AddNote/AddNote";
 import ApiContext from '../ApiContext';
 import config from '../config';
 import './App.css';
-<<<<<<< HEAD
-=======
-import ErrorBoundary from "../ErrorBoundary/ErrorBoundary";
->>>>>>> 0330a561bc6e4c5c0ea5f03b15f36d2a131eb3e7
 
 class App extends Component {
     state = {
@@ -41,23 +31,15 @@ class App extends Component {
                 return Promise.all([notesRes.json(), foldersRes.json()]);
             })
             .then(([notes, folders]) => {
-<<<<<<< HEAD
                 this.setState({ notes, folders });
             })
             .catch(error => {
                 console.error({ error });
-=======
-                this.setState({notes, folders});
-            })
-            .catch(error => {
-                console.error({error});
->>>>>>> 0330a561bc6e4c5c0ea5f03b15f36d2a131eb3e7
             });
     }
 
     addFolder = folder => {
         this.setState({
-<<<<<<< HEAD
             folders: [...this.state.folders, folder],
         })
     }
@@ -67,17 +49,6 @@ class App extends Component {
             notes: [...this.state.notes, note],
         })
     }
-=======
-          folders: [...this.state.folders, folder],
-        })
-      }
-
-    addNote = note => {
-        this.setState({
-          notes: [...this.state.notes, note],
-        })
-      }
->>>>>>> 0330a561bc6e4c5c0ea5f03b15f36d2a131eb3e7
     handleDeleteNote = noteId => {
         this.setState({
             notes: this.state.notes.filter(note => note.id !== noteId)
@@ -95,24 +66,13 @@ class App extends Component {
                         component={NoteListNav}
                     />
                 ))}
-<<<<<<< HEAD
                 <Route path="/note/:noteId" component={NotePageNav} />
                 <Route path="/add-folder" component={NotePageNav} />
                 <Route path="/add-note" component={NotePageNav} />
-=======
-                <ErrorBoundary><Route path="/note/:noteId" component={NotePageNav} /></ErrorBoundary>
-                <ErrorBoundary><Route path="/add-folder" component={NotePageNav} /></ErrorBoundary>
-                <ErrorBoundary><Route path="/add-note" component={NotePageNav} /></ErrorBoundary>
->>>>>>> 0330a561bc6e4c5c0ea5f03b15f36d2a131eb3e7
             </>
         );
     }
 
-<<<<<<< HEAD
-
-=======
-    
->>>>>>> 0330a561bc6e4c5c0ea5f03b15f36d2a131eb3e7
     renderMainRoutes() {
         return (
             <>
@@ -124,15 +84,9 @@ class App extends Component {
                         component={NoteListMain}
                     />
                 ))}
-<<<<<<< HEAD
                 <Route path="/note/:noteId" component={NotePageMain} />
                 <Route path="/add-folder" component={AddFolder} />
                 <Route path="/add-note" component={AddNote} />
-=======
-                <ErrorBoundary><Route path="/note/:noteId" component={NotePageMain} /></ErrorBoundary>
-                <ErrorBoundary><Route path="/add-folder" component={AddFolder} /></ErrorBoundary>
-                <ErrorBoundary><Route path="/add-note" component={AddNote} /></ErrorBoundary>
->>>>>>> 0330a561bc6e4c5c0ea5f03b15f36d2a131eb3e7
             </>
         );
     }
