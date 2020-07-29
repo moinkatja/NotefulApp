79eed83ff3da29031a45195f38dd3d9c046db660
Katja <55636811+moinkatja@users.noreply.github.com>
2020-07-29 21:56:31 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -8,21 +8,14 @@ import { BrowserRouter } from 'react-router-dom'
 import 'typeface-roboto'
 import './index.css'
 import App from './App/App'
-<<<<<<< HEAD
 import "./ErrorBoundary/ErrorBoundary";
 import ErrorBoundary from './ErrorBoundary/ErrorBoundary'
-=======
->>>>>>> 0330a561bc6e4c5c0ea5f03b15f36d2a131eb3e7
 
 library.add(faPlus, faChevronLeft, faTrashAlt, faCheckDouble)
 
 ReactDOM.render(
   <BrowserRouter>
-<<<<<<< HEAD
     <ErrorBoundary><App /></ErrorBoundary>
-=======
-    <App />
->>>>>>> 0330a561bc6e4c5c0ea5f03b15f36d2a131eb3e7
   </BrowserRouter>,
   document.getElementById('root')
 )
